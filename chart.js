eee14354947926aa360604e280c531d8df5b7831
3dw1nM0535 <workockmoses@gmail.com>
2017-10-08 11:59:49 +0300

    Added datasets and label options

--- chart.js
+++ chart.js
@@ -1,1 +1,27 @@
 var myChart = document.getElementById('myChart').getContext('2d');
+
+var chartPlot = new Chart(myChart, {
+  type: 'bar',
+  data: {
+    labels: ['Worcester city', 'Lowel city', 'Cambridge city', 'Quincy city', 'Lynn city', 'Newton city', 'Lawrence city', 'Somerville city', 'Brokton city', 'Springfield city', 'Malden city', 'Plymouth city', 'Medford city'],
+    datasets: [{
+      label: 'Population',
+      data: [
+        614437,
+        342483,
+        223431,
+        122209,
+        89234,
+        43233,
+        56988,
+        77882,
+        34445,
+        57889,
+        23342,
+        45542,
+        22234
+      ]
+    }],
+  },
+  options: {},
+});
